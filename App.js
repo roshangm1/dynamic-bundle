4aa4ed35eb2ccbe3ad561937c69d2049208aaf0f
Roshan Gautam <roshangm01@gmail.com>
2022-05-03 17:08:32 +0200

    name

--- App.js
+++ App.js
@@ -84,7 +84,7 @@ const App = () => {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <View style={{padding: 16}}>
-          <Text>I am Roshan Gautam</Text>
+          <Text>I am Roshan</Text>
         </View>
         <Button title="Download" onPress={downloadPdf} />
       </ScrollView>
