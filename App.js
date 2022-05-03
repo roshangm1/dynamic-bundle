9a2842a323c123b11a73181180f76ae01c3611dc
Roshan Gautam <roshangm01@gmail.com>
2022-05-03 17:27:03 +0200

    double

--- App.js
+++ App.js
@@ -85,6 +85,7 @@ const App = () => {
         style={backgroundStyle}>
         <View style={{padding: 16}}>
           <Text>I am Roshan</Text>
+          <Text>I am Roshan</Text>
         </View>
         <Button title="Download" onPress={downloadPdf} />
       </ScrollView>
